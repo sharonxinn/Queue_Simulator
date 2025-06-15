95244253a3d784f7a3655ef3b34c5377ad5df77d
KerJingKJ <jingkerchan@gmail.com>
2025-06-15 10:02:16 +0800

    update the way to display (to be add with the functions later)

--- display.m
+++ display.m
@@ -1,6 +1,6 @@
 function display()
     fprintf('Welcome to the Petrol Station Simulator! \n');
-    numVechicles = input('Enter the number of vehicles: ');
+    numVehicles = input('Enter the number of vehicles: ');
     random_nums = 0;
     
     fprintf('There are two random number generator: \n');
@@ -8,33 +8,34 @@ function display()
     fprintf('2. Multiplicative Distribution \n');
     type_of_generator = input('Enter 1 or 2: ');
     if type_of_generator == 1
-        random_nums = lcg(3 * numVechicles);
+        random_nums = lcg(3 * numVehicles); % we need generate 3 sets of random number
         
     elseif type_of_generator == 2
-        random_nums = multiplicative(3 * numVechicles);
+        random_nums = multiplicative(3 * numVehicles);
     end
-           
+    
+    %prepare a list to store the random number
+    interArrival = zeros(1, numVehicles);
+    petrolType = zeros(1, numVehicles);
+    refuelTime = zeros(1, numVehicles);
+    
+    for i = 1:numVehicles
+        idx = (i - 1) * 3 + 1;
+        interArrival(i) = random_nums(idx);
+        petrolType(i) = random_nums(idx + 1);
+        refuelTime(i) = random_nums(idx + 2);
+    end
+    
     fprintf('Random number for inter-arrival time: ');
-    for i=1:numVechicles
-        idx = (i-1)*3 + 1;
-        rand1 = random_nums(idx);   % For inter-arrival
-        fprintf('%d  ', rand1);
-    end
+    fprintf('%d  ', interArrival);
     fprintf('\n');
-    
+
     fprintf('Random number for petrol type       : ');
-    for i=1:numVechicles
-        idx = (i-1)*3 + 1;
-        rand2 = random_nums(idx+1); % For petrol type
-        fprintf('%d  ', rand2);
-    end
+    fprintf('%d  ', petrolType);
     fprintf('\n');
-    
+
     fprintf('Random number for refueling time    : ');
-    for i=1:numVechicles
-        idx = (i-1)*3 + 1;
-        rand3 = random_nums(idx+2); % For refuel time
-        fprintf('%d  ', rand3);        
-    end
+    fprintf('%d  ', refuelTime);
     fprintf('\n');
+            
  end
