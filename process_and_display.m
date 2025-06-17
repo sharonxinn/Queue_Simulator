820a510247ae2f6f7cf34102b8488387f8829f91
KerJingKJ <jingkerchan@gmail.com>
2025-06-17 13:55:14 +0800

    Update process_and_display.m

--- process_and_display.m
+++ process_and_display.m
@@ -2,16 +2,7 @@ function process_and_display(data, label)
     if isempty(data)
         return;
     end
-
-    fprintf('\n=== %s ===\n', label);
-    fprintf('%-139s| %-100s\n', '', '                    Pump 1:');
-    fprintf(['%-10s | %-11s | %-13s | %-17s | %-18s | %-13s | %-13s | %-4s | ', ...
-             '%-15s | %-15s | %-12s | %-9s\n'], ...
-            'Vehicle No', 'Petrol Type', 'Quantity (L)', 'Total Price (RM)', ...
-            'Rand Interarrival', 'Interarrival', 'Arrival Time', 'Line', ...
-            'Rand Refueling', 'Refueling Time', 'Time Begins', 'Time Ends');
-    fprintf('%s\n', repmat('-', 1, 190));
-
+    
     pump_end_times = zeros(1, 4);
     pump_data = cell(size(data,1), 14);
 
@@ -21,8 +12,9 @@ function process_and_display(data, label)
         refuel = generate_refuel_time(rand_refuel);
         assigned_pump = 0;
 
-        % FreeMat-compatible random line assignment
+        % Random line assignment
         line_no = floor(1 + rand() * 2);
+        data{i,16} = line_no;
         if line_no == 1
             line_pumps = [1, 2];
         else
@@ -50,6 +42,7 @@ function process_and_display(data, label)
         data{i,12} = end_time;
         data{i,13} = wait_time;
         data{i,14} = time_spent;
+        data{i,15} = assigned_pump; 
 
         for p = 1:4
             if p == assigned_pump
@@ -64,27 +57,44 @@ function process_and_display(data, label)
         end
         pump_data{i,13} = wait_time;
         pump_data{i,14} = time_spent;
-
+        
+    end
+    
+    %display logs%
+    display_logs(data);
+    
+    
+    %display table%
+    fprintf('\n=== %s ===\n', label);
+    fprintf('%-139s| %-100s\n', '', '                    Pump 1:');
+    fprintf(['%-10s | %-11s | %-13s | %-17s | %-18s | %-13s | %-13s | %-4s | ', ...
+             '%-15s | %-15s | %-12s | %-9s\n'], ...
+            'Vehicle No', 'Petrol Type', 'Quantity (L)', 'Total Price (RM)', ...
+            'Rand Interarrival', 'Interarrival', 'Arrival Time', 'Line', ...
+            'Rand Refueling', 'Refueling Time', 'Time Begins', 'Time Ends');
+    fprintf('%s\n', repmat('-', 1, 190));
+    
+     for i = 1:size(data,1)
         inter_display = '-';
         if i ~= 1
             inter_display = num2str(data{i,7});
         end
 
         % Only show refuel details in Pump 1 column if assigned to Pump 1
-        if assigned_pump == 1
-            refuel_str = num2str(refuel);
-            begin_str = num2str(begin_time);
-            end_str = num2str(end_time);
+        if data{i,15} == 1
+            refuel_str = num2str(data{i,10});
+            begin_str = num2str(data{i,11});
+            end_str = num2str(data{i,12});
         else
             refuel_str = '-';
             begin_str = '-';
             end_str = '-';
-        end
+        end       
 
         fprintf(['%10d | %-11s | %13.2f | %17.2f | %18d | %13s | %13d | %4d | ', ...
                  '%15d | %-15s | %-12s | %-9s\n'], ...
                 data{i,2}, data{i,3}, data{i,4}, data{i,5}, data{i,6}, inter_display, ...
-                data{i,8}, line_no, data{i,9}, refuel_str, begin_str, end_str);
+                data{i,8}, data{i,16}, data{i,9}, refuel_str, begin_str, end_str);
     end
 
     fprintf('\n\n');
